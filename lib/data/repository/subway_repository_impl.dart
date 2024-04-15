a13cfd24e0ef62bbf698b4fafee54db31e75bb57
hoogom88 <ziziwhdgus@naver.com>
2024-04-15 11:33:30 +0900

    전종현 2

--- lib/data/repository/subway_repository_impl.dart
+++ lib/data/repository/subway_repository_impl.dart
@@ -1,12 +1,24 @@
 import 'package:flutter_subway/data/data_source/subway_data_source.dart';
+import 'package:flutter_subway/data/mapper/subway_mapper.dart';
 import 'package:flutter_subway/data/model/realtime_arrival.dart';
 import 'package:flutter_subway/data/repository/subway_repository.dart';
 
 class SubwayRepositoryImpl implements SubwayRepository {
-  final SubwayDataSource dataSource;
+  final SubwayDataSource _dataSource;
 
-  SubwayRepositoryImpl({required this.dataSource});
+  SubwayRepositoryImpl({required SubwayDataSource dataSource})
+      : _dataSource = dataSource;
 
   @override
-  Future<List<RealtimeArrival>> getRealtimeArrivalList(String key) {}
+  Future<List<RealtimeArrival>> getRealtimeArrivalList(String key) async {
+    List<RealtimeArrival> _result = [];
+    final dto = await _dataSource.getSubwayResult(key);
+
+    if (dto.realtimeArrivalDtos != null &&
+        dto.realtimeArrivalDtos!.isNotEmpty) {
+      _result =
+          dto.realtimeArrivalDtos!.map((e) => e.toRealtimeArrival()).toList();
+    }
+    return _result;
+  }
 }
---
+++ lib/presentation/home_viewModel.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_subway/data/model/realtime_arrival.dart';
+
+import '../data/repository/subway_repository.dart';
+
+class HomeViewModel with ChangeNotifier {
+  final SubwayRepository _repository;
+
+  HomeViewModel({required repository}) : _repository = repository;
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  RealtimeArrival? _upSubway;
+  RealtimeArrival? _downSubway;
+
+  RealtimeArrival? get upSubway => _upSubway;
+
+  RealtimeArrival? get downSubway => _downSubway;
+
+  Future<void> onSearch(String key) async {
+    changeLoadingStatus(true);
+
+    List<RealtimeArrival> result =
+        await _repository.getRealtimeArrivalList(key);
+
+    final arrivals = result
+        .where((e) => e.arvlCd == '0' && e.arvlCd == '3')
+        .toList();
+    arrivals.sort((old, now) =>
+        ((int.tryParse(now.barvlDt) ?? 0) - (int.tryParse(old.barvlDt) ?? 0)));
+    
+    _upSubway = arrivals.firstWhere((e) => e.updnLine == '상행');
+    _downSubway = arrivals.firstWhere((e) => e.updnLine == '하행');
+
+    changeLoadingStatus(false);
+  }
+
+  void changeLoadingStatus(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+}
